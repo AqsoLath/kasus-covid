5e95c6d4d89559b87a3ac4f78ef4da0f2d55d2c9
unknown <AxoLath@gmail.com>
2021-06-01 21:06:51 +0700

    menambahkan css dan drop box

--- kasuscovid.php
+++ kasuscovid.php
@@ -1,8 +1,8 @@
 <?php 
 
-// $data = file_get_contents("https://api.kawalcorona.com/indonesia");
+$data = file_get_contents("https://api.kawalcorona.com/indonesia");
 
-$data = file_get_contents("indonesia.json");
+// $data = file_get_contents("indonesia.json");
 
 $data2 = file_get_contents("https://api.kawalcorona.com/indonesia/provinsi");
 
@@ -21,39 +21,40 @@ $provinsi = json_decode($data2, true);
 <!DOCTYPE html>
 <html>
 <head>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 	<title>kasus korona</title>
 
-	<link rel="stylesheet" type="text/css" href="style1.css">
+	<link rel="stylesheet" type="text/css" href="style2.css">
 </head>
 <body>
 
 <div class="container">
 	<div class="indonesia">
 		<h1>Kasus Covid19 Indonesia</h1>
-		<div class="data-indonesia">
-			<div class="kasus-indonesia">
-				<h2>positif </h2> 
-				<h3><?php echo($korona[0]["positif"]) ?> </h3>
+		<div class="data">
+			<div class="kasus positif">
+				<h3>Positif </h3> 
+				<h4><?php echo($korona[0]["positif"]) ?> </h4>
 			</div>
-			<div class="kasus-indonesia">
-				<h2>meninggoy </h2>
-				<h3> <?php echo($korona[0]["meninggal"]) ?> </h3>
+			<div class="kasus meninggal">
+				<h3>Meninggoy </h3>
+				<h4> <?php echo($korona[0]["meninggal"]) ?> </h4>
 			</div>
-			<div class="kasus-indonesia">
-				<h2>sembuh </h2> 
-				<h3><?php echo($korona[0]["sembuh"]) ?> </h3>
+			<div class="kasus sembuh">
+				<h3>Sembuh </h3> 
+				<h4><?php echo($korona[0]["sembuh"]) ?> </h4>
 			</div>
 		</div>
 	</div>
 
-
 	<h1 class="judul-daerah">Kasus Covid19 Daerah</h1>
 	<div class="daerah" >
-		
-		<div class="container1">
-			<form action="" method="post">
+			<form action="" method="post" class="form-provinsi">
 				<!-- <input type="text" min="0" step="1" name="kode" placeholder="Masukkan kode daerah"> -->
 				<label for="provinsi">Pilih Provinsi: </label>
+				<div class="longkap"></div>
 				<select id="provinsi" name="kode">
 
 					<?php
@@ -68,20 +69,29 @@ $provinsi = json_decode($data2, true);
 					<?php endfor ?>
 					
 				</select>
-				<input type="submit" name="submit">
+				<input type="submit" name="submit" value="Lihat">
 			</form>
 
 			<?php if (isset($_POST['submit'])): ?>
-
 				<?php 
 				 $int = (is_numeric($_POST['kode']) ? (int)$_POST['kode'] : 0);
 				 // var_dump($int); ?>
-				<h2> provinsi :  <?php echo $provinsi[$int]["attributes"]["Provinsi"] ; ?> </h2>
-				<h3> Positif :  <?php echo $provinsi[$int]["attributes"]["Kasus_Posi"] ; ?> </h3>
-				<h3> Meninggal :  <?php echo $provinsi[$int]["attributes"]["Kasus_Meni"] ; ?> </h3>
-				<h3> Sembuh :  <?php echo $provinsi[$int]["attributes"]["Kasus_Semb"] ; ?> </h3>
+				<h2> Provinsi   <?php echo $provinsi[$int]["attributes"]["Provinsi"] ; ?> </h2>
+				<div class="data data">
+					<div class="kasus positif kasus-daerah">
+					 	<h3> Positif  </h3>
+						<h4> <?php echo $provinsi[$int]["attributes"]["Kasus_Posi"] ; ?> </h4>
+					 </div>
+					 <div class="kasus meninggal kasus-daerah">
+					 	<h3> Meninggal </h3>
+						<h4>  <?php echo $provinsi[$int]["attributes"]["Kasus_Meni"] ; ?> </h4>
+					 </div>
+					 <div class="kasus sembuh kasus-daerah">
+					 	<h3> Sembuh </h3> 
+						<h4> <?php echo $provinsi[$int]["attributes"]["Kasus_Semb"] ; ?></h4>
+					 </div>	
+				</div>
 			<?php endif ?>
-		</div>
 	</div>
 </div>
 </body>

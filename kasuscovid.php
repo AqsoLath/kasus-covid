306f3f2f39514c964d82c253007361db96175442
unknown <AxoLath@gmail.com>
2021-06-09 09:53:30 +0700

    menambahkan bootstrap

--- kasuscovid.php
+++ kasuscovid.php
@@ -4,13 +4,13 @@
 // var_dump(error_get_last());
 
 
-$data = file_get_contents("https://api.kawalcorona.com/indonesia");
+// $data = file_get_contents("https://api.kawalcorona.com/indonesia");
 
-// $data = file_get_contents("indonesia.json");
+$data = file_get_contents("indonesia.json");
 
-$data2 = file_get_contents("https://api.kawalcorona.com/indonesia/provinsi");
+// $data2 = file_get_contents("https://api.kawalcorona.com/indonesia/provinsi");
 
-// $data2 = file_get_contents("daerah.json");
+$data2 = file_get_contents("daerah.json");
 
 
 
@@ -41,72 +41,78 @@ $provinsi = json_decode($data2, true);
 	<title>kasus korona</title>
 
 	<link rel="stylesheet" type="text/css" href="style3.css">
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 </head>
 <body>
 
 	<a href="refresh.php" class="error-button refresh">Refresh</a>
 
 <div class="container">
-	<div class="indonesia">
-		<h1>Kasus Covid19 Indonesia</h1>
-		<div class="data">
-			<div class="kasus positif">
-				<h3>Positif </h3> 
-				<h4><?php echo($korona[0]["positif"]) ?> </h4>
-			</div>
-			<div class="kasus meninggal">
-				<h3>Meninggal </h3>
-				<h4> <?php echo($korona[0]["meninggal"]) ?> </h4>
-			</div>
-			<div class="kasus sembuh">
-				<h3>Sembuh </h3> 
-				<h4><?php echo($korona[0]["sembuh"]) ?> </h4>
+	<div class="kasus-all">
+		<div class="indonesia">
+			<h1>Kasus Covid19 Indonesia</h1>
+			<div class="data">
+				<div class="kasus positif">
+					<h3>Positif </h3> 
+					<h4><?php echo($korona[0]["positif"]) ?> </h4>
+				</div>
+				<div class="kasus meninggal">
+					<h3>Meninggal </h3>
+					<h4> <?php echo($korona[0]["meninggal"]) ?> </h4>
+				</div>
+				<div class="kasus sembuh">
+					<h3>Sembuh </h3> 
+					<h4><?php echo($korona[0]["sembuh"]) ?> </h4>
+				</div>
 			</div>
 		</div>
+
+		<h1 class="judul-daerah">Kasus Covid19 Daerah</h1>
+		<div class="daerah" >
+				<form action="" method="post" class="form-provinsi">
+					<label for="provinsi">Pilih Provinsi: </label>
+					<div class="longkap"></div>
+					<select id="provinsi" name="kode">
+
+						<?php
+						$i = 0;
+						 foreach ($provinsi as $p): ?>
+							
+							
+							<?php $i++ ?>
+						<?php endforeach ?>
+						<?php for ($i=0; $i < 1; $i++) :?> 
+							<option value="<?php echo $i ?>"> <?php echo $provinsi[$i]["attributes"]["Provinsi"]; ?> </option>
+						<?php endfor ?>
+						
+					</select>
+					<input type="submit" name="submit" value="Lihat">
+				</form>
+
+				<?php if (isset($_POST['submit'])): ?>
+					<?php 
+					 $int = (is_numeric($_POST['kode']) ? (int)$_POST['kode'] : 0);
+					  ?>
+					<h2> Provinsi   <?php echo $provinsi[$int]["attributes"]["Provinsi"] ; ?> </h2>
+					<div class="data data">
+						<div class="kasus positif kasus-daerah">
+						 	<h3> Positif  </h3>
+							<h4> <?php echo $provinsi[$int]["attributes"]["Kasus_Posi"] ; ?> </h4>
+						 </div>
+						 <div class="kasus meninggal kasus-daerah">
+						 	<h3> Meninggal </h3>
+							<h4>  <?php echo $provinsi[$int]["attributes"]["Kasus_Meni"] ; ?> </h4>
+						 </div>
+						 <div class="kasus sembuh kasus-daerah">
+						 	<h3> Sembuh </h3> 
+							<h4> <?php echo $provinsi[$int]["attributes"]["Kasus_Semb"] ; ?></h4>
+						 </div>	
+					</div>
+				<?php endif ?>
+		</div>
 	</div>
-
-	<h1 class="judul-daerah">Kasus Covid19 Daerah</h1>
-	<div class="daerah" >
-			<form action="" method="post" class="form-provinsi">
-				<label for="provinsi">Pilih Provinsi: </label>
-				<div class="longkap"></div>
-				<select id="provinsi" name="kode">
-
-					<?php
-					$i = 0;
-					 foreach ($provinsi as $p): ?>
-						
-						
-						<?php $i++ ?>
-					<?php endforeach ?>
-					<?php for ($i=0; $i < 34; $i++) :?> 
-						<option value="<?php echo $i ?>"> <?php echo $provinsi[$i]["attributes"]["Provinsi"]; ?> </option>
-					<?php endfor ?>
-					
-				</select>
-				<input type="submit" name="submit" value="Lihat">
-			</form>
-
-			<?php if (isset($_POST['submit'])): ?>
-				<?php 
-				 $int = (is_numeric($_POST['kode']) ? (int)$_POST['kode'] : 0);
-				  ?>
-				<h2> Provinsi   <?php echo $provinsi[$int]["attributes"]["Provinsi"] ; ?> </h2>
-				<div class="data data">
-					<div class="kasus positif kasus-daerah">
-					 	<h3> Positif  </h3>
-						<h4> <?php echo $provinsi[$int]["attributes"]["Kasus_Posi"] ; ?> </h4>
-					 </div>
-					 <div class="kasus meninggal kasus-daerah">
-					 	<h3> Meninggal </h3>
-						<h4>  <?php echo $provinsi[$int]["attributes"]["Kasus_Meni"] ; ?> </h4>
-					 </div>
-					 <div class="kasus sembuh kasus-daerah">
-					 	<h3> Sembuh </h3> 
-						<h4> <?php echo $provinsi[$int]["attributes"]["Kasus_Semb"] ; ?></h4>
-					 </div>	
-				</div>
-			<?php endif ?>
+	<div class="pencegahan">
 	</div>
 </div>
 

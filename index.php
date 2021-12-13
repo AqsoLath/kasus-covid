1ace84359160a0b68e7ef54e4b511f5b62d7fa84
AXO <77832984+AxoLath@users.noreply.github.com>
2021-12-14 06:40:02 +0700

    Ngehapus bug ada tulisan error

--- index.php
+++ index.php
@@ -305,26 +305,6 @@ $provinsi = json_decode($data2, true);
 
 
 
-
-<?php 
-
-if (null !== error_get_last()) : ?>
-
-	<!-- <button ></button> -->
-	<div class="container">
-		<div class="error">
-			<div class="box-error">
-				<h6>Ada Masalah</h6>
-				<a href="refresh.php" class="error-button">REFRESH</a>
-			</div>
-		</div>
-	</div>
-	
-	
-	<!-- header("Location: refresh.php"); -->
-
-<?php endif ?>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
